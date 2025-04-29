0f1f5a95e126a9961c9ad7a89e297337199cbb1b
Hokma <gubaryura02@gmail.com>
2025-04-29 13:05:48 +0300

    App changing, css deleted

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const IDS = [
+  { id: 1, nick: 'nick1' },
+  { id: 2, nick: 'nick2' },
+  { id: 3, nick: 'nick3' },
+  { id: 4, nick: 'nick4' },
+  { id: 5, nick: 'nick5' },
+];
+const AUTHORS = [
+  {id:1, login: 'login1'},
+  {id:2, login: 'login2'},
+  {id:3, login: 'login3'},
+  {id:4, login: 'login4'},
+  {id:5, login: 'login5'}
+];
 function App() {
   const [count, setCount] = useState(0)
 
@@ -21,13 +35,31 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>Edit <code>src/App.tsx</code> and save to test HMR</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="authors">
+        {AUTHORS.map((author)=>(
+          <div key ={author.id}>
+            <h2>{author.login}</h2>
+            <p>ID:{author.id}</p>
+          </div>
+        ))
+
+        }
+      </div>
+      
+      <div className = "ideas">
+        {IDS.map((idea)=>(
+          <div key = {idea.id}>
+            <h2>{idea.nick}</h2>
+            <p>ID: {idea.id}</p>
+          </div>
+        ))
+        }
+      </div>
     </>
   )
 }
